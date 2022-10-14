7edc3522f2d53914050b24de517aec78fe5c3440
eblyncho <eblyn52@naver.com>
2022-10-14 16:39:49 +0900

    Vector 사용하기

---
+++ src/Board.java
@@ -0,0 +1,11 @@
+public class Board {
+    String subject;
+    String content;
+    String writer;
+
+    public Board(String subject, String content, String writer) {
+        this.subject = subject;
+        this.content = content;
+        this.writer = writer;
+    }
+}
